65dab5bd6db76ffdebd83a3360f749de751cb26b
Mesayaaa <mesayachristopher2112@gmail.com>
2025-02-11 17:10:13 +0700

    Update places.ts

--- app/data/places.ts
+++ app/data/places.ts
@@ -4,7 +4,6 @@ export interface Place {
   id: number;
   name: string;
   image: string;
-  description: string;
   mapsLink: string;
 }
 
@@ -13,35 +12,30 @@ export const places: Place[] = [
     id: 1,
     name: "Borobudur Temple",
     image: getImagePath("/images/borobudur.jpg"),
-    description: "The largest Buddhist temple in the world, located in Magelang, Central Java.",
     mapsLink: "https://goo.gl/maps/QKQYYZqx6YdNgzjZ6"
   },
   {
     id: 2,
     name: "Raja Ampat",
     image: getImagePath("/images/borobudur.jpg"),
-    description: "Beautiful archipelago known for its rich marine biodiversity in West Papua.",
     mapsLink: "https://goo.gl/maps/xMHQgGZ8qH8LFNRP6"
   },
   {
     id: 3,
     name: "Mount Bromo",
     image: getImagePath("/images/borobudur.jpg"),
-    description: "An active volcano in East Java known for its spectacular sunrise views.",
     mapsLink: "https://goo.gl/maps/L4c8Vt8zoQYoUqWb7"
   },
   {
     id: 4,
     name: "Tanah Lot",
     image: getImagePath("/images/borobudur.jpg"),
-    description: "Ancient Hindu temple perched on a rock formation in Bali.",
     mapsLink: "https://goo.gl/maps/YQxYyZH8xvYKFDFR9"
   },
   {
     id: 5,
     name: "Pink Beach",
     image: getImagePath("/images/borobudur.jpg"),
-    description: "Unique pink-sand beach in Komodo National Park.",
     mapsLink: "https://goo.gl/maps/xX3vX8TQQkqhJ7Lz8"
   }
 ]; 
